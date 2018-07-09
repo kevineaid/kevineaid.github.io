b8a11f0d94e8fadb053ba526ee9c1a28344ad9e3
kevineaid <kevineaid@users.noreply.github.com>
2018-07-09 11:18:34 +0700

    update folder

--- js/core.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-
-    //Nav Toggle Button 
-    $('.nav-toggle').click(function(){
-        $(this).toggleClass('nav-toggle-clicked');
-        $('.sidebar-nav').toggleClass('nav-showed');
-    }); 
-    
-});
