88f348fb5ca78755bc973a2aa8f7232395fc7c21
Ibnu Suhaemy <ibnu@clloh.com>
2018-09-24 22:19:13 +0700

    a

--- demo/rowers/js/main.js
+++ demo/rowers/js/main.js
@@ -51,6 +51,15 @@ $(document).ready(function(){
     $('#trigger-popup').click(function() {
         $('#pop-up').fadeToggle(400);
     });
+
+    var isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
+        if (window.location.hash && isChrome) {
+            setTimeout(function () {
+                var hash = window.location.hash;
+                window.location.hash = "";
+                window.location.hash = hash;
+            }, 300);
+        }
 });
 
 
